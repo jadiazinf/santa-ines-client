9c111824975d0b9a2a02c522be70123c3fd98bee
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-07-28 13:51:03 -0700

    Feature/Footer

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,14 @@
 import menu from "./svg/menu.svg";
 import close from "./svg/close.svg";
+import instagram from "./svg/instagram.svg";
+import facebook from "./svg/facebook.svg";
+import linkedin from "./svg/linkedin.svg";
+import twitter from "./svg/twitter.svg";
+import adress_pointer from "./svg/adress_pointer.svg";
+import phone from "./svg/phone.svg";
+import mail from "./svg/mail.svg";
 import logo_SantaInes from "./png/logo_SantaInes.png"
+import footer_SantaInes from "./png/footer_SantaInes.png"
+import footer_SantaInes_2 from "./png/footer_SantaInes_2.png"
 
-export { menu, close, logo_SantaInes}
+export { menu, close, instagram, facebook, linkedin, twitter, adress_pointer, phone, mail, logo_SantaInes, footer_SantaInes, footer_SantaInes_2}
