7cc2d304f943a16fe8bf792d7b73daea10b5fb1b
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-07-31 14:04:53 -0700

    feature/Quienes_Somos_Component

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export * from './Navbar/navbar.component'
 export * from './datatables/datatable.component';
 export * from '/footer/footer.component';
 export * from './inputs/input.component';
+export * from './quienes_somos/quienes_somos.component';
 
