2783f6814b016e560e61f59be7053cf1aeabba43
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-08-01 17:06:36 -0700

    Feature/Botones de Vanguardia section y Fix/Areglando estilos

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,11 +8,13 @@ import adress_pointer from "./svg/adress_pointer.svg";
 import phone from "./svg/phone.svg";
 import mail from "./svg/mail.svg";
 import arrowNext from "./svg/arrowNext.svg";
+import calendar from "./svg/calendar.svg";
+import teamIcon from "./svg/teamIcon.svg";
+import cash from "./svg/cash.svg";
 import logo_SantaInes from "./png/logo_SantaInes.png"
 import footer_SantaInes from "./png/footer_SantaInes.png"
 import footer_SantaInes_2 from "./png/footer_SantaInes_2.png"
 import section_Qsomos from "./png/section_Qsomos_doc.png"
 import teamSI from "./png/teamSI.png"
-import teamSI2 from "./png/teamSI2.png"
 
-export { menu, close, instagram, facebook, linkedin, twitter, adress_pointer, phone, mail, arrowNext, logo_SantaInes, footer_SantaInes, footer_SantaInes_2, section_Qsomos, teamSI, teamSI2}
+export { menu, close, instagram, facebook, linkedin, twitter, adress_pointer, phone, mail, arrowNext, calendar, teamIcon, cash, logo_SantaInes, footer_SantaInes, footer_SantaInes_2, section_Qsomos, teamSI}
--- src/pages/indexpage/index.page.jsx
+++ src/pages/indexpage/index.page.jsx
@@ -1,7 +1,5 @@
 import { useSelector } from "react-redux";
-import { VanguiardiaComponent } from "../../components";
-import NavbarComponent from "../../components/Navbar/navbar.component";
-import FooterComponent from "../../components/footer/footer.component";
+import { LoginForm } from "../../components";
 
 export const IndexPage = () => {
 
@@ -27,12 +25,11 @@ export const IndexPage = () => {
   // }
 
   return (
-    <div className="px-2">
-      <NavbarComponent/>
+    <>
+      {/* navbar */}
       {/* main */}
-      {/* <h1>USER ROLE: { role }</h1> */}
-      <VanguiardiaComponent/>
-      <FooterComponent/>
-    </div>
+      {/* footer */}
+      <h1>USER ROLE: { role }</h1>
+    </>
   );
 }
