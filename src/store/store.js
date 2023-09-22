97a452fdee18f8675dd6bffb38104eb21fb8a68f
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-09-22 00:02:49 -0700

    feat(New Slice): :sparkles: Slice para la informacion de la cita que esta siendo creada

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { doctorsApi, loginUserApi, registerUserApi } from "../api";
 import userReducer from "./reducers/user.reducer";
+import appointmentReducer from "./reducers/crearCita.reducer";
 
 export const store = configureStore({
   reducer: {
     [doctorsApi.reducerPath]: doctorsApi.reducer,
-    authenticatedUser: userReducer
+    authenticatedUser: userReducer,
+    createAppointment: appointmentReducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware()
                                         .concat(doctorsApi.middleware,
