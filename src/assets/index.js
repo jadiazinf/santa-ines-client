4c70164f66ffd10b6a7da2a20b0518a80fa049db
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-10-10 22:11:32 -0700

    feat(Assets): :bento: Add new assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -17,5 +17,7 @@ import footer_SantaInes from "./png/footer_SantaInes.png"
 import footer_SantaInes_2 from "./png/footer_SantaInes_2.png"
 import section_Qsomos from "./png/section_Qsomos_doc.png"
 import teamSI from "./png/teamSI.png"
+import delete_icon from "./svg/delete_icon.svg"
+import edit_icon from "./svg/edit_icon.svg"
 
-export { menu, close, instagram, facebook, linkedin, twitter, adress_pointer, phone, mail, arrowNext, calendar, teamIcon, cash, map, logo_SantaInes, footer_SantaInes, footer_SantaInes_2, section_Qsomos, teamSI}
+export { menu, close, instagram, facebook, linkedin, twitter, adress_pointer, phone, mail, arrowNext, calendar, teamIcon, cash, map, logo_SantaInes, footer_SantaInes, footer_SantaInes_2, section_Qsomos, teamSI, delete_icon, edit_icon}
