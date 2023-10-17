5e9e0d5e14887dd1c31b82a99f732752b72c3c77
Ashly <avct2001@gmail.com>
2023-10-17 18:15:07 -0400

    Funcionamiento botón "otros servicios"

--- src/components/servicios/infoServicios.jsx
+++ src/components/servicios/infoServicios.jsx
@@ -1,29 +1,44 @@
+import React, { useState } from 'react';
+import ServiciosComponent from './servicios.component';
 
-//Info especifica servicios
+// Info específica del servicio
 const ServicioInfo = () => {
+  const [mostrarServicios, setMostrarServicios] = useState(false);
+  const handleClick = () => {
+    setMostrarServicios(true);
+    console.log('Botón clickeado');
+  };
 
   return (
     <section id="servicios" className="flex flex-col justify-center mx-10 my-10">
 
-      <div className="flex flex-col justify-center items-center mb-8">
-        <div className="flex flex-col items-center w-[50%]">
-          <h1 className="text-secondary text-[15px] font-poppins font-bold">SIEMPRE ATENTOS</h1>
-          <h1 className="text-primary text-[25px] font-poppins font-bold mb-4">Servicios</h1>
-          <p className="text-center text-[14px] font-poppins mb-2">El horario de atención es de lunes a viernes de 7:00 A.M. a 4:00 P.M.</p>
-          <p className="text-center text-[14px] font-poppins mb-2">Las solicitudes de citas las puede realizar en la sección <button className="font-bold text-primary">agendar cita</button></p>
-          <p className="text-center text-[14px] font-poppins mb-2">Si requiere más información que no esté publcada en nuestra página web o redes sociales puede llamar de lunes a viernes de 8:00 A.M. a 3:00 P.M. al número local 0212.471.12.66</p>
+      {!mostrarServicios && (
+        <div className="flex flex-col justify-center items-center mb-8">
+          <div className="flex flex-col items-center w-[50%]">
+            <h1 className="text-secondary text-[15px] font-poppins font-bold">SIEMPRE ATENTOS</h1>
+            <h1 className="text-primary text-[25px] font-poppins font-bold mb-4">Servicios</h1>
+            <p className="text-center text-[14px] font-poppins mb-2">El horario de atención es de lunes a viernes de 7:00 A.M. a 4:00 P.M.</p>
+            <p className="text-center text-[14px] font-poppins mb-2">Las solicitudes de citas las puede realizar en la sección <button className="font-bold text-primary">agendar cita</button></p>
+            <p className="text-center text-[14px] font-poppins mb-2">Si requiere más información que no esté publicada en nuestra página web o redes sociales puede llamar de lunes a viernes de 8:00 A.M. a 3:00 P.M. al número local 0212.471.12.66</p>
+          </div>
         </div>
-      </div>
+      )}
 
-      <div className="flex flex-col mb-8">
-        <h1 className="text-dimBlue text-[25px] font-poppins font-bold mb-4">Titulo del servicio</h1>
-        <p className="flex flex-col items-justify text-center text-[14px] font-poppins mb-2">Información detallada del servicio</p>
-      </div>
+      {!mostrarServicios && (
+        <div className="flex flex-col mb-8">
+          <h1 className="text-dimBlue text-[25px] font-poppins font-bold mb-4">Titulo del servicio</h1>
+          <p className="flex flex-col items-justify text-center text-[14px] font-poppins mb-2">Información detallada del servicio</p>
+        </div>
+      )}
 
-      <button className="font-bold text-primary ml-auto">Ver más servicios</button>
+      {mostrarServicios && <ServiciosComponent />}
+
+      {!mostrarServicios && (
+        <button className="font-bold text-primary ml-auto" onClick={handleClick}>Ver otros servicios</button>
+      )}
 
     </section>
-  )
-}
+  );
+};
 
 export default ServicioInfo
