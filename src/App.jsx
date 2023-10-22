3f001a7152e5fdf16474c71b036620532ba29440
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-10-22 00:02:48 -0700

    refactor(Refactor): :recycle: Add Routes and Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useSelector } from "react-redux"
-import { RouterProvider } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { notAuthenticatedUserRoutes,
          adminUserRoutes,
          regularUserRoutes,
          superUserRoutes } from "./routes";
+import { Toaster } from "react-hot-toast";
+import { IndexPage, LoginPage } from "./pages";
 
 export const App = () => {
 
@@ -30,7 +32,15 @@ export const App = () => {
 
   return (
     <>
-      <RouterProvider router={userRoutes}/>
+      {/* <RouterProvider router={userRoutes}/> */}
+      <Routes>
+        <Route  path="/" element={<IndexPage />} />
+        <Route  path="/login" element={<LoginPage />} />
+      </Routes>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
     </>
   );
 }
