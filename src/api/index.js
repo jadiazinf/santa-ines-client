02540046f4ed7bf879a552a2f765dda9b0cbca0e
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-10-21 20:21:44 -0700

    refactor(Refactor): :recycle: Add new endpoints

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
 export * from './doctors.api';
 export * from './register-user.api';
 export * from './login-user.api';
+export { useLoginUserMutation, useGetAllUsersMutation, useGetInfoUserMutation, useCreateUserMutation, useUpdateUserMutation, useDeleteUserMutation } from './user.api';
