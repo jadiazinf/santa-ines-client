c5c9bff78207f9e437671b1d29e383374dfa4ae1
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-10-22 00:02:23 -0700

    refactor(Refactor): :recycle: Add a BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { App } from './App.jsx'
 import { store } from './store';
 import './index.css';
 import {NextUIProvider} from "@nextui-org/react";
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <NextUIProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </NextUIProvider>
     </Provider>
   </React.StrictMode>
