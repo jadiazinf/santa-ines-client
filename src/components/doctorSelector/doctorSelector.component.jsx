1e6fff0a375e1cbfad20812ecf60024ec1187b48
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-10-22 01:16:26 -0700

    refactor(Refactor): :recycle: Add new NavLink logic

--- src/components/doctorSelector/doctorSelector.component.jsx
+++ src/components/doctorSelector/doctorSelector.component.jsx
@@ -5,7 +5,7 @@ import { crearCitaDescripcion, crearCitaDoctor } from '../../store/reducers/crea
 import { useDispatch } from 'react-redux';
 
 
-export const DoctorSelector = ({ setReady }) => {
+export const DoctorSelector = ({ setDoctor }) => {
   const [doctorSelected, setDoctorSelected] = useState([]);
   const [doctoresSeleccionados, setDoctoresSeleccionados] = useState([]); // Usamos un array para mantener los doctores seleccionados
   const dispatch = useDispatch()
@@ -15,7 +15,9 @@ export const DoctorSelector = ({ setReady }) => {
       setDoctorSelected([]);
       setDoctoresSeleccionados([]);
       dispatch(crearCitaDoctor({}));
+      setDoctor({})
     } else {
+      setDoctor(doctor)
       dispatch(crearCitaDoctor(doctor));
       setDoctorSelected(doctor);
       setDoctoresSeleccionados([{id:doctor.id, nombre:doctor.nombre.primerNombre, especialidad:doctor.especialidad}]);
@@ -66,40 +68,125 @@ export const DoctorSelector = ({ setReady }) => {
       "genero": "Femenino",
       "correo": "maria.rodriguez@example.com",
       "id": "D54321"
+    },
+    {
+      "nombre": {
+        "primerNombre": "Juan",
+        "segundoNombre": "Carlos"
+      },
+      "apellido": {
+        "primerApellido": "Gómez",
+        "segundoApellido": "Pérez"
+      },
+      "fechaNacimiento": {
+        "dia": 10,
+        "mes": 5,
+        "anio": 1980
+      },
+      "especialidad": "Cardiología",
+      "cedula": "123456789",
+      "direccion": "Calle 123, Ciudad",
+      "telefono": "555-123-4567",
+      "genero": "Masculino",
+      "correo": "juan.gomez@example.com",
+      "id": "D123445"
+    },
+    {
+      "nombre": {
+        "primerNombre": "María",
+        "segundoNombre": "Elena"
+      },
+      "apellido": {
+        "primerApellido": "Rodríguez",
+        "segundoApellido": "López"
+      },
+      "fechaNacimiento": {
+        "dia": 15,
+        "mes": 8,
+        "anio": 1975
+      },
+      "especialidad": "Dermatología",
+      "cedula": "987654321",
+      "direccion": "Avenida XYZ, Ciudad",
+      "telefono": "555-987-6543",
+      "genero": "Femenino",
+      "correo": "maria.rodriguez@example.com",
+      "id": "D5423321"
+    },
+    {
+      "nombre": {
+        "primerNombre": "Juan",
+        "segundoNombre": "Carlos"
+      },
+      "apellido": {
+        "primerApellido": "Gómez",
+        "segundoApellido": "Pérez"
+      },
+      "fechaNacimiento": {
+        "dia": 10,
+        "mes": 5,
+        "anio": 1980
+      },
+      "especialidad": "Cardiología",
+      "cedula": "123456789",
+      "direccion": "Calle 123, Ciudad",
+      "telefono": "555-123-4567",
+      "genero": "Masculino",
+      "correo": "juan.gomez@example.com",
+      "id": "D145"
+    },
+    {
+      "nombre": {
+        "primerNombre": "María",
+        "segundoNombre": "Elena"
+      },
+      "apellido": {
+        "primerApellido": "Rodríguez",
+        "segundoApellido": "López"
+      },
+      "fechaNacimiento": {
+        "dia": 15,
+        "mes": 8,
+        "anio": 1975
+      },
+      "especialidad": "Dermatología",
+      "cedula": "987654321",
+      "direccion": "Avenida XYZ, Ciudad",
+      "telefono": "555-987-6543",
+      "genero": "Femenino",
+      "correo": "maria.rodriguez@example.com",
+      "id": "D534321"
     }
   ]
 
   return (
-    <div className="flex flex-col gap-1 m-10">
-      <CheckboxGroup
-        value={doctoresSeleccionados.map((doctor) => doctor.id)}
-        classNames={{
-          base: "w-full",
-        }}
-      >
-        {doctores.map((doctor, index) => (
-          <CustomCheckbox
-            key={index}
-            value={doctor.id}
-            user={{
-              name: `${doctor.nombre.primerNombre} ${doctor.apellido.primerApellido}`,
-              avatar: "https://avatars.githubusercontent.com/u/30373425?v=4",
-              username: `${doctor.correo}`,
-              role: doctor.especialidad,
-              status: "Active",
+    <div className="grid xs:grid-cols-1 smm:grid-cols-2 gap-4 m-10">
+      {doctores.map((doctor, index) => (
+        <div key={index} className="flex flex-col gap-1" >
+          <CheckboxGroup
+            value={doctoresSeleccionados.map((doctor) => doctor.id)}
+            classNames={{
+              base: "w-full",
             }}
-            statusColor="primary"
-            checked={doctorSelected && doctorSelected.id === doctor.id}
-            onChange={() => handleCheckboxChange(doctor)}
-          />
-        ))}
-      </CheckboxGroup>
-      {/* {doctoresSeleccionados.length > 0 
-      ? <p className="mt-4 ml-1 text-default-500">
-          Selected: {doctoresSeleccionados.map((doctor) => doctor.id).join(", ")}
-        </p>
-      : null
-      } */}
+          >
+            <CustomCheckbox
+              value={doctor.id}
+              user={{
+                name: `${doctor.nombre.primerNombre} ${doctor.apellido.primerApellido}`,
+                avatar: "https://avatars.githubusercontent.com/u/30373425?v=4",
+                username: `${doctor.correo}`,
+                role: doctor.especialidad,
+                status: "Active",
+              }}
+              statusColor="primary"
+              checked={doctorSelected && doctorSelected.id === doctor.id}
+              onChange={() => handleCheckboxChange(doctor)}
+            />
+          </CheckboxGroup>
+          {/* Puedes agregar cualquier otro contenido que desees mostrar en cada columna */}
+        </div>
+      ))}
     </div>
+
   );
 };
