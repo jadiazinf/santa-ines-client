0ca84e56a6c72b53f07bcbc35613442ee9a66a3c
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-10-22 22:07:33 -0700

    feat(new): :sparkles: Add new API

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
-export * from './doctors.api';
+export * from './doctor.api';
 export * from './register-user.api';
 export * from './login-user.api';
 export { useLoginUserMutation, useGetAllUsersMutation, useGetInfoUserMutation, useCreateUserMutation, useUpdateUserMutation, useDeleteUserMutation } from './user.api';
+export { useGetDoctorsMutation, useGetDoctors1Mutation } from './doctor.api';
