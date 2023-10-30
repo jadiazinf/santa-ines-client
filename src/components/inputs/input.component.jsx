2ec82263bc36162e13958c5c34659e5b131dc776
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-10-29 19:42:05 -0700

    refactor(Refactor): :recycle: New input selectedState

--- src/components/inputs/input.component.jsx
+++ src/components/inputs/input.component.jsx
@@ -1,24 +1,24 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const InputComponent = (props) => {
+    const [isSelected, setIsSelected] = useState(false);
 
-  const {id, name, placeholder, onChange, type, onBlur, value, error} = props;
-  //Deberia tener un estado para saber si esta seleccionado o no el campo, que lo puedo tener en el padre
-  // de esta manera podemos mostrar o no el span de color verde
-  const selectedDiv =  true;
-
-  //De esta manera tenemos una referencia al input, y cada que se seleccione el componente completo, se va a seleccionar el input
+  const {id, name, placeholder, onChange, type, onBlur, value, error, className1} = props;
   const inputRef = useRef();
-  const handleClick = () => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
+
+  const handleInputClick = () => {
+    setIsSelected(true);
+    inputRef.current.focus();
+  };
+
+  const handleInputBlur = () => {
+    setIsSelected(false);
   };
 
   return (
-    <div className=' w-[550px] border border-gray-500 flex flex-col relative ' onClick={handleClick}>
-      <div>
-      <span className={`${selectedDiv ? 'bg-green-400' : ''} w-1`}></span>
+    <div className={`${className1 ? className1 : 'w-[550px]'} border border-gray-500 flex flex-col relative `} onClick={handleInputClick}>
+      <div className="flex flex-row">
+        <span className={`${isSelected ? 'bg-green-400 duration-300' : ''} w-1`}></span>
         <div className="flex flex-col justify-center p-2 w-full">
           <span className="text-textTitle">{placeholder}</span>
           <input
@@ -28,7 +28,7 @@ export const InputComponent = (props) => {
             name={name}
             type={type}
             onChange={onChange}
-            onBlur={onBlur}
+            onBlur={handleInputBlur}
             value={value}
           />
         </div>
