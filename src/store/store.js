0010660a5ef0f037967deb792b8e431dc7a7fed0
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-11-10 20:13:46 -0800

    Refactor store.js to use Redux slices.

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { appointmentsApi, doctorsApi, loginUserApi, registerUserApi } from "../api";
+import { appointmentsApi, doctorsApi, registerUserApi } from "../api";
 import { userApi } from "../api/user.api";
-import userReducer from "./reducers/user.reducer";
-import appointmentReducer from "./reducers/crearCita.reducer";
+import userSlice from "./reducers/user.reducer";
+import appointmentSlice from "./reducers/crearCita.reducer";
+import doctorsSlice from "./reducers/doctors.reducer";
+import userAdminSlice from "./reducers/userAdmin.reducer";
+import appointmentEditSlice from "./reducers/editarCita.reducer";
+import appointmentDetalleSlice from "./reducers/detalleCita.reducer";
 
 export const store = configureStore({
   reducer: {
-    [loginUserApi.reducerPath]: loginUserApi.reducer,
     [registerUserApi.reducerPath]: registerUserApi.reducer,
     [doctorsApi.reducerPath]: doctorsApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
     [appointmentsApi.reducerPath]: appointmentsApi.reducer,
-    authenticatedUser: userReducer,
-    createAppointment: appointmentReducer,
+    authenticatedUser: userSlice,
+    createAppointment: appointmentSlice,
+    saveDoctors: doctorsSlice,
+    userAdmin: userAdminSlice,
+    editAppointment: appointmentEditSlice,
+    detalleAppointment: appointmentDetalleSlice,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware()
                                         .concat(doctorsApi.middleware,
                                                 registerUserApi.middleware,
-                                                loginUserApi.middleware,
                                                 userApi.middleware,
                                                 appointmentsApi.middleware
                                                 )
