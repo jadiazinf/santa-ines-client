957229d425805fa2011ab0c0cbbef93a9a09a413
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-11-10 20:13:34 -0800

    Add appointment form page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export * from './indexPage/index.page';
 export * from './loginPage/login.page';
 export * from './dashboardPage/dashboard.page';
 export * from './doctorInfoPage/doctorInfo.page';
+export * from './appointmentPage/appointmentForm.page';
