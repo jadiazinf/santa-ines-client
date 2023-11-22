1492ac353450d592b549be24b3149bca968d1c61
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-11-22 15:13:56 -0800

    Add ButtonBack component and capitalize doctor's name

--- src/pages/doctorInfoPage/doctorInfo.page.jsx
+++ src/pages/doctorInfoPage/doctorInfo.page.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { DoctorInfo, FilledButton, TabsComponent } from '../../components';
+import { ButtonBack, DoctorInfo, FilledButton, TabsComponent } from '../../components';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { TableComponent } from '../../components/table/table.component';
+import { capitalizeFirstLetter } from '../../helpers/capitalize.helper';
 
 export const DoctorInfoPage = () => {
   const { appointments } = useSelector( state => state.createAppointment)
@@ -29,11 +30,14 @@ export const DoctorInfoPage = () => {
   ];
 
   return (
-    <section className="flex flex-col items-center">
+    <section className="flex flex-col items-center mt-5 gap-5">
       <div className='flex justify-between w-[80%]'>
-        <div className=''>
-          <h1 className='text-primary text-3xl '>{doctor.nombre.cuerpo}</h1>
-          <p className=''>Por favor, seleccione la pestaña que desee consultar</p>
+        <div className='flex flex-row justify-center items-center gap-5'>
+          <ButtonBack  style={''}/>
+          <div>
+            <h1 className='text-primary text-3xl w-[400px]'>{capitalizeFirstLetter(doctor.nombre.cuerpo)}</h1>
+            <p className=''>Por favor, seleccione la pestaña que desee consultar.</p>
+          </div>
         </div>
         <FilledButton text='Agendar Cita' onClick={() => onClick()} />
       </div>
