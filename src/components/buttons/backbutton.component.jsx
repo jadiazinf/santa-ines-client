278c96155385e81df5af158159acda16fa8f4a71
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-11-22 15:13:33 -0800

    Fix handleGoBack function to navigate to a specific route

--- src/components/buttons/backbutton.component.jsx
+++ src/components/buttons/backbutton.component.jsx
@@ -1,13 +1,13 @@
 import { useNavigate } from "react-router-dom";
 
-export const ButtonBack = ({ style }) => {
+export const ButtonBack = ({ style, onClick }) => {
   const navigate = useNavigate();
 
   const handleGoBack = () => {
     navigate(-1); //TODO -> Tratar de cambiar esto para que vaya a una ruta especifica la DASHBOARD
   };
   return (
-    <button onClick={handleGoBack} type="button" className={`${style} text-primary border border-primary hover:bg-green-600 hover:text-white  font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2`}>
+    <button onClick={handleGoBack || onClick} type="button" className={`${style} text-primary border border-primary hover:bg-green-600 hover:text-white  font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2`}>
       <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
         <path stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7"/>
       </svg>
