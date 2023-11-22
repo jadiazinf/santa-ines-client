7508ddea7b23f3a2a6bae53659d0a68e23554bfb
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-11-22 13:31:39 -0800

    Refactor perfil page component and add user info retrieval

--- src/pages/perfilPage/perfil.page.jsx
+++ src/pages/perfilPage/perfil.page.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import { PerfilComponent } from '../../components'
-import { Image } from '@nextui-org/react'
-import { backgroundPerfil } from '../../assets'
+import React, { useEffect, useState } from 'react'
+import { ButtonBack, PerfilComponent } from '../../components'
+import { useGetInfoUserMutation } from '../../api';
+import { useDispatch, useSelector } from 'react-redux';
+import { editUser } from '../../store/reducers/user.reducer';
 
-export const PerfilPage = () => {
+export const PerfilPage = ({ action}) => {
+  const dispatch = useDispatch();
+  const [getInfoUser, { isLoading, isError }] = useGetInfoUserMutation();
+  const { username } = useSelector( state => state.authenticatedUser)
+  useEffect(() => {
+    getInfoUser(username)
+    .then((response) => {
+      dispatch(editUser(response.data));
+    })
+    .catch((error) => {
+      reject(new Error(response.data.message));
+    })
+  }, [])
+
   return (
-    <section className="flex flex-col justify-center items-center m-10">
-      <div className=''>
-        <Image src={ backgroundPerfil } alt="background" className='z-10 scale-90'/>
-        <Image src='https://avatars.githubusercontent.com/u/30373425?v=4' alt="user" width={200} className='absolute rounded-full -bottom-20 left-10 z-20'/>
+    <section className="flex flex-col justify-start items-start m-10">
+      <div className='flex flex-row justify-center items-center ml-20 gap-5'>
+        <ButtonBack  style={''}/>
+        <div>
+          <h1 className='text-primary text-3xl w-[400px]'>Edicion de {action}</h1>
+          <p className=''>Seleccione el doctor a consultar.</p>
+        </div>
       </div>
-      <PerfilComponent />
+      <PerfilComponent/>
     </section>
   )
 }
