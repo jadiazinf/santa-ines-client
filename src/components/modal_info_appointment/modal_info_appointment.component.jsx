a45695bf9ac3e9e9511f8ffd47b5d990bc0b107a
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-12-01 19:32:43 -0800

    Refactor modal component and add user form

--- src/components/modal_info_appointment/modal_info_appointment.component.jsx
+++ src/components/modal_info_appointment/modal_info_appointment.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Chip} from "@nextui-org/react";
 import { useDispatch, useSelector } from 'react-redux';
-import { FilledButton } from '../buttons/filledbutton.component';
 import { capitalizeFirstLetter } from '../../helpers/capitalize.helper';
 import { format } from 'date-fns';
 import { es } from "date-fns/locale";
 import { fechaHora } from '../../helpers/calendar.helper';
 import { setAccion } from '../../store/reducers/detalleCita.reducer';
+import { FilledButton, UserForm } from '../../components'
+
 
 const statusColorMap = {
   Activa: "primary",
@@ -15,9 +16,12 @@ const statusColorMap = {
   Completada: "success",
 };
 
-export const ModalInfoComponent = ({ isOpen, onOpenChange }) => {
+export const ModalInfoComponent = ({ isOpen, onOpenChange, setReset }) => {
   const dispatch = useDispatch();
   const { accion } = useSelector( state => state.detalles)
+  const detalles = ['cita', 'usuario', 'doctor', 'paciente'];
+  const acciones = ['crearUsuario', 'crearDoctor', 'crearPaciente'];
+
 
   const handleClick = () => {
     dispatch(setAccion());
@@ -25,21 +29,12 @@ export const ModalInfoComponent = ({ isOpen, onOpenChange }) => {
 
   return (
     <>
-      <Modal  backdrop={'blur'} isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={true}  className=' bg-slate-50 p-2 rounded-lg'>
+      <Modal  backdrop={'blur'} isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={true}  className=' bg-slate-50 p-2 rounded-lg' size={detalles.includes(accion) ? '' : '2xl'}>
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1 font-bold text-lg text-primary underline">Información de {accion}:</ModalHeader>
-              <ModalBody>
-                { accion === 'cita' ? <DetalleCita /> : null }
-                { accion === 'usuario' ? <DetalleUsuario /> : null }
-                { accion === 'doctor' ? <DetalleDoctor /> : null }
-                { accion === 'paciente' ? <DetallePaciente /> : null }
-
-              </ModalBody>
-              <ModalFooter>
-                <FilledButton onClick={() => { handleClick(); onClose(); }} class={`bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded w-[100px]`} text={'Cerrar'}/>
-              </ModalFooter>
+              { detalles.includes(accion) ? <ModalViewInfoComponent accion={accion} handleClick={handleClick} onClose={onClose}/> : null}
+              { acciones.includes(accion) ? <ModalActionsComponent accion={accion} handleClick={handleClick} onClose={onClose} setReset={setReset}/> : null}
             </>
           )}
         </ModalContent>
@@ -47,6 +42,42 @@ export const ModalInfoComponent = ({ isOpen, onOpenChange }) => {
     </>
   )
 }
+
+const ModalActionsComponent = ({ accion, handleClick, onClose, setReset }) => {
+  const subString = accion.substring("crear".length)
+  return(
+    <>
+      <ModalHeader className="flex flex-col gap-1 font-bold text-lg text-primary underline">Creación de {subString}:</ModalHeader>
+      <ModalBody>
+        { accion === 'crearUsuario' ? <UserForm acction={'crear'} onClose={onClose} handleClick={handleClick} setReset={setReset}/> : null }
+        {/* { accion === 'crearDoctor' ? <DoctorForm /> : null }*/}                   {/* aca se colocan los componentes que crea Ashly*/}
+        {/* { accion === 'crearPaciente' ? <PacienteForm /> : null } */}
+      </ModalBody>
+    </>
+  )
+}
+
+
+const ModalViewInfoComponent = ({ accion, handleClick, onClose }) => {
+  return(
+    <>
+      <ModalHeader className="flex flex-col gap-1 font-bold text-lg text-primary underline">Información de {accion}:</ModalHeader>
+      <ModalBody>
+        { accion === 'cita' ? <DetalleCita /> : null }
+        { accion === 'usuario' ? <DetalleUsuario /> : null }
+        { accion === 'doctor' ? <DetalleDoctor /> : null }
+        { accion === 'paciente' ? <DetallePaciente /> : null }
+
+      </ModalBody>
+      <ModalFooter>
+        <FilledButton onClick={() => { handleClick(); onClose(); }} class={`bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded w-[100px]`} text={'Cerrar'}/>
+      </ModalFooter>
+    </>
+  )
+}
+
+
+
 
 
 //TODO -> Este componente se puede mejorar para que no se repita tanto codigo
