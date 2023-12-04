c65598ff8427be8f6a334c4571430e5810573d0a
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-12-04 11:10:53 -0800

    Fix date picker styling and add dropdowns

--- src/components/select-date/select-date.jsx
+++ src/components/select-date/select-date.jsx
@@ -30,13 +30,20 @@ export const SelectDateComponent = (props) => {
           <DatePicker
             ref={selectDateRef}
             className='text-textInput border-none outline-none w-full bg-transparent'
+
             id={id}
             name={name}
             selected={value}
             onChange={handleDateChange}
             onBlur={handleSelectDateBlur}
+            
             dateFormat='yyyy-MM-dd'
             calendarClassName='custom-calendar'
+            maxDate={new Date()}
+
+            showMonthDropdown
+            showYearDropdown
+            dropdownMode="select"
           />
         </div>
       </div>
