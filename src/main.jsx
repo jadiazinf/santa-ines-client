8a087321a0287defb13b28f5d0253788c2470b56
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-12-04 13:06:07 -0800

    Update component import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,13 @@ import { store } from './store';
 import './index.css';
 import {NextUIProvider} from "@nextui-org/react";
 import { BrowserRouter } from 'react-router-dom';
-import { DoctorForm } from './components/doctor-form/doctor-form.component.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <NextUIProvider>
         <BrowserRouter>
-          <DoctorForm />
+          <App />
         </BrowserRouter>
       </NextUIProvider>
     </Provider>
