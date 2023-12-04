fb01e61410081c3791eef5ba9bdc13205784c959
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-12-04 14:05:24 -0800

    Remove especialidadValidation from editDoctorSchema

--- src/validations/edit-doctor.validattions.js
+++ src/validations/edit-doctor.validattions.js
@@ -4,7 +4,6 @@ import { firstNameValidation, lastNameValidation, especialidadValidation, cedula
 export const editDoctorSchema = Yup.object({
   nombre : firstNameValidation,
   apellido : lastNameValidation,
-  especialidad: especialidadValidation,
   cedula: cedulaValidation,
   telefono: phoneValidation,
   genero: generoValidation,
