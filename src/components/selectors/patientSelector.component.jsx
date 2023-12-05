80ca61b9530d6962d06f34518f97fb08d70bcf44
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-12-04 18:17:55 -0800

    Add patient selection functionality and button to open modal

--- src/components/selectors/patientSelector.component.jsx
+++ src/components/selectors/patientSelector.component.jsx
@@ -3,9 +3,11 @@ import { CheckboxGroup } from '@nextui-org/react'
 import { CustomCheckbox } from './customCheckbox';
 import { crearPatient } from '../../store/reducers/crearCita.reducer';
 import { useDispatch, useSelector } from 'react-redux';
+import { FilledButton } from '../../components'
+import { setAccion2 } from '../../store/reducers/detalleCita.reducer';
 
 
-export const PatientSelector = ({ patientEditable }) => {
+export const PatientSelector = ({ patientEditable, onOpen }) => {
   const [patientSelected, setPatientSelected] = useState(null);
   const dispatch = useDispatch()
   const { patients } = useSelector( state => state.saveDoctors)
@@ -26,37 +28,44 @@ export const PatientSelector = ({ patientEditable }) => {
       dispatch(crearPatient(paciente))
     }
   };
-
+const onClickOpen = () => {
+  dispatch(setAccion2('crearPacienteCita'));
+}
   return (
-    <div className="grid xs:grid-cols-1 smm:grid-cols-2 gap-4 mt-24 m-10">
-      {patients === undefined
-        ? <h1>Aún no tienes asignados pacientes agregardos al sistema, agrega uno.</h1>  //TODO -> Agregar modal de ashly para agregar paciente
-        :<>
-          {Object.values(patients).map((paciente, index) => (
-            <div key={index} className="flex flex-col gap-1" >
-              <CheckboxGroup
-                value={patientSelected ? [patientSelected.id_number] : []}
-                classNames={{
-                  base: "w-full",
-                }}
-              >
-                <CustomCheckbox
-                  value={paciente.id_number}
-                  user={{
-                    name: `${paciente.name} ${paciente.lastname}`,
-                    avatar: "https://avatars.githubusercontent.com/u/30373425?v=4",
-                    username: `${paciente.id_number}`,
-                    status: "Active",
+    <div className="mt-14 m-10 flex flex-col justify-end items-end">
+      <FilledButton onClick={() => { onClickOpen(); onOpen(); }}  text={'Agregar paciente'} type='button'/>
+      <div className='grid xs:grid-cols-1 smm:grid-cols-2 gap-4 '>
+        {patients === undefined
+          ? <>
+              <h1>Aún no tienes asignados pacientes agregardos al sistema, agrega uno.</h1>
+            </>
+          :<>
+            {Object.values(patients).map((paciente, index) => (
+              <div key={index} className="flex flex-col gap-1" >
+                <CheckboxGroup
+                  value={patientSelected ? [patientSelected.id_number] : []}
+                  classNames={{
+                    base: "w-full",
                   }}
-                  statusColor="primary"
-                  checked={patientSelected && patientSelected.id_number === paciente.id_number}
-                  onChange={() => handleCheckboxChange(paciente)}
-                />
-              </CheckboxGroup>
-            </div>
-          ))}
-        </>
-      }
+                >
+                  <CustomCheckbox
+                    value={paciente.id_number}
+                    user={{
+                      name: `${paciente.name} ${paciente.lastname}`,
+                      avatar: "https://avatars.githubusercontent.com/u/30373425?v=4",
+                      username: `${paciente.id_number}`,
+                      status: "Active",
+                    }}
+                    statusColor="primary"
+                    checked={patientSelected && patientSelected.id_number === paciente.id_number}
+                    onChange={() => handleCheckboxChange(paciente)}
+                  />
+                </CheckboxGroup>
+              </div>
+            ))}
+          </>
+        }
+      </div>
     </div>
 
   );
