90f94cf4600033e8104aeb2b9a31b10839559d80
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-12-18 18:19:13 -0800

    Add SearchBarComponent

---
+++ src/components/searchBar/searchBar.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { SearchIcon } from '../../assets';
+
+export const SearchBarComponent = ({searchValue, setSearchValue}) => {
+  const onSearchValueChange = (event) => {
+    setSearchValue(event.target.value);
+  }
+  return(
+    <div className="h-8 w-[300px] flex-row justify-between items-center border py-1 px-3 gap-1 rounded  flex">
+      <input
+        placeholder="Search"
+        value={searchValue}
+        onChange={onSearchValueChange}
+        className="text-black font-normal text-sm outline-none w-full h-4 placeholder:text-secondary-200"
+      />
+      <SearchIcon />
+    </div>
+  );
+}
