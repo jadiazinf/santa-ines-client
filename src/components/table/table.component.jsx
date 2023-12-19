73416e794a2097a7d054faedb34b38fd896c0618
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-12-18 16:47:11 -0800

    Add pagination functionality to table component

--- src/components/table/table.component.jsx
+++ src/components/table/table.component.jsx
@@ -79,10 +79,34 @@ export const TableComponent = ({columns, id_doctor, action, data, path, setReset
     }
   }
 
+  //------------------> Pagination Config <------------------
+  const [page, setPage] = React.useState(1);
+  const rowsPerPage = 5;
+  const totalItems = data.length;
+
+  data = React.useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+    return data.slice(start, end);
+  }, [page, data]);
+  //------------------> Pagination Config <------------------
+
   return (
     <>
       <ModalInfoComponent isOpen={isOpen} onOpen={onOpen} onOpenChange={onOpenChange} setReset={setReset}/>
-      <Table aria-label="tabla" className="">
+      <Table
+        aria-label="tabla"
+        bottomContent={
+          <div className="flex w-full justify-center mt-4">
+            <CustomPagination
+              totalItems={totalItems}
+              itemsPerPage={rowsPerPage}
+              currentPage={page}
+              onPageChange={(newPage) => setPage(newPage)}
+            />
+        </div>
+        }
+      >
         <TableHeader columns={columns} className="mb-5">
           {(column) => (
             <TableColumn className="bg-gray-200 " key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
@@ -119,4 +143,54 @@ const contentSelection = (action, item, columnKey, id_doctor, onClick, onOpen, d
     default:
       return content = <div>Contenido por defecto</div>;
   }
+}
+
+const CustomPagination = ({ totalItems, itemsPerPage, currentPage, onPageChange }) => {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      onPageChange(newPage);
+    }
+  };
+
+  const renderizarComponente = () => {
+    const componentes = [];
+    for (let i = 1; i <= totalPages; i++) {
+      componentes.push(<SpamComponent key={i} numberPage={i} currentPage={currentPage} />);
+    }
+    return componentes;
+  };
+
+  return (
+    <section aria-label="Page navigation example">
+      <ul className="flex items-center -space-x-px h-8 text-sm">
+        <li>
+          <button className={`flex items-center justify-center px-3 h-8 ms-0 leading-tight text-black-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700`} onClick={() => handlePageChange(currentPage - 1)} >
+            <span className="sr-only">Previous</span>
+            <svg className="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
+            </svg>
+          </button>
+        </li>
+        {renderizarComponente()}
+        <li>
+          <button className={`flex items-center justify-center px-3 h-8 leading-tight text-black-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 `} onClick={() => handlePageChange(currentPage + 1)} >
+            <span className="sr-only">Next</span>
+            <svg className="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
+            </svg>
+          </button>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+const SpamComponent = ({ numberPage, currentPage }) => {
+  return (
+    <li>
+      <span className={`flex items-center justify-center px-3 h-8 leading-tight text-black-500 border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ${currentPage === numberPage ? 'bg-primary text-white ' : 'bg-white '}`}>{numberPage}</span>
+    </li>
+  )
 }
