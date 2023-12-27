ee16173e9ab2be5ac1158399d45a782870b5e16d
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-12-27 14:34:16 -0800

    Add search functionality to DoctorSelector component

--- src/components/selectors/doctorSelector.component.jsx
+++ src/components/selectors/doctorSelector.component.jsx
@@ -4,6 +4,7 @@ import { CustomCheckbox } from './customCheckbox';
 import { crearCitaDoctor } from '../../store/reducers/crearCita.reducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { savedoctor } from '../../store/reducers/doctors.reducer';
+import { SearchBarComponent } from '../searchBar/searchBar.component';
 
 
 export const DoctorSelector = () => {
@@ -27,39 +28,59 @@ export const DoctorSelector = () => {
   };
   // Este es el array de doctores que sera tomado de la API
 
-  return (
-    <div className="grid xs:grid-cols-1 smm:grid-cols-2 gap-4 mt-24 m-10">
-      {doctors === undefined
-        ? <h1>Aún no tienes asignados doctores, por favor ponte en contacto con el administrador.</h1>
-        :<>
-          {Object.values(doctors).map((doctor, index) => (
-            <div key={index} className="flex flex-col gap-1" >
-              <CheckboxGroup
-                value={doctoresSeleccionados.map((doctor) => doctor.cedula)}
-                classNames={{
-                  base: "w-full",
-                }}
-              >
-                <CustomCheckbox
-                  value={doctor.cedula}
-                  user={{
-                    name: `${doctor.nombre.cuerpo} ${doctor.apellido.cuerpo}`,
-                    avatar: "https://avatars.githubusercontent.com/u/30373425?v=4",
-                    username: `${doctor.correo.correo}`,
-                    role: doctor.especialidad,
-                    status: "Active",
-                  }}
-                  statusColor="primary"
-                  checked={doctorSelected && doctorSelected.cedula === doctor.cedula}
-                  onChange={() => handleCheckboxChange(doctor)}
-                />
-              </CheckboxGroup>
-              {/* Puedes agregar cualquier otro contenido que desees mostrar en cada columna */}
-            </div>
-          ))}
-        </>
-      }
-    </div>
+  const [searchValue, setSearchValue] = React.useState('');
+  let searchedDatas = [];
 
+  if (searchValue.length === 0){
+    searchedDatas = doctors;
+  }else{
+    searchedDatas = doctors.filter(item => {
+      const itemTextName = item.nombre.cuerpo.toLowerCase();
+      const itemTextApellido = item.apellido.cuerpo.toLowerCase();
+      const itemTextCorreo = item.correo.correo.toLowerCase();
+      const itemTextCedula = item.cedula.toLowerCase();
+      const itemTextespecialidad = item.especialidad.toLowerCase();
+      return itemTextName.includes(searchValue) || itemTextApellido.includes(searchValue) || itemTextCorreo.includes(searchValue) || itemTextCedula.includes(searchValue) || itemTextespecialidad.includes(searchValue);
+    });
+  }
+
+  return (
+    <div className='flex flex-col justify-center items-end mt-10'>
+      <SearchBarComponent searchValue={searchValue} setSearchValue={setSearchValue}/>
+      <div className={`grid xs:grid-cols-1 ${searchedDatas.length !== 0 ? 'smm:grid-cols-2' : 'smm:grid-cols-1'} gap-5  m-10`}>
+        {searchedDatas.length === 0
+          ? <div className='flex flex-col justify-center items-center '>
+              <h1 className='font-bold text-lg text-primary'>No se ha encontrado ningún doctor con las características especificadas.</h1>
+            </div>
+          :<>
+            {Object.values(searchedDatas).map((doctor, index) => (
+              <div key={index} className="flex flex-col gap-1" >
+                <CheckboxGroup
+                  value={doctoresSeleccionados.map((doctor) => doctor.cedula)}
+                  classNames={{
+                    base: "w-full",
+                  }}
+                >
+                  <CustomCheckbox
+                    value={doctor.cedula}
+                    user={{
+                      name: `${doctor.nombre.cuerpo} ${doctor.apellido.cuerpo}`,
+                      avatar: "https://avatars.githubusercontent.com/u/30373425?v=4",
+                      username: `${doctor.correo.correo}`,
+                      role: doctor.especialidad,
+                      status: "Active",
+                    }}
+                    statusColor="primary"
+                    checked={doctorSelected && doctorSelected.cedula === doctor.cedula}
+                    onChange={() => handleCheckboxChange(doctor)}
+                  />
+                </CheckboxGroup>
+                {/* Puedes agregar cualquier otro contenido que desees mostrar en cada columna */}
+              </div>
+            ))}
+          </>
+        }
+      </div>
+    </div>
   );
 };
