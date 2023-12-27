04199407d004044c83261d7beef3aafb540f6cd1
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-12-27 14:34:02 -0800

    Update search bar height and input field height

--- src/components/searchBar/searchBar.component.jsx
+++ src/components/searchBar/searchBar.component.jsx
@@ -6,12 +6,12 @@ export const SearchBarComponent = ({searchValue, setSearchValue}) => {
     setSearchValue(event.target.value);
   }
   return(
-    <div className="h-8 w-[300px] flex-row justify-between items-center border py-1 px-3 gap-1 rounded  flex">
+    <div className="h-9 w-[300px] flex-row justify-between items-center border py-1 px-3 gap-1 rounded  flex">
       <input
         placeholder="Search"
         value={searchValue}
         onChange={onSearchValueChange}
-        className="text-black font-normal text-sm outline-none w-full h-4 placeholder:text-secondary-200"
+        className="text-black font-normal text-sm outline-none w-full h-5 placeholder:text-secondary-200"
       />
       <SearchIcon />
     </div>
