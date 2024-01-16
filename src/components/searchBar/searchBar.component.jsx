0b56fde16b266a34a929c8ceb356e4bd8c226e21
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-01-16 13:41:50 -0800

    Update SearchBarComponent to conditionally render SearchIcon

--- src/components/searchBar/searchBar.component.jsx
+++ src/components/searchBar/searchBar.component.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { SearchIcon } from '../../assets';
 
-export const SearchBarComponent = ({searchValue, setSearchValue}) => {
+export const SearchBarComponent = ({searchValue, setSearchValue, visible}) => {
   const onSearchValueChange = (event) => {
     setSearchValue(event.target.value);
   }
   return(
     <div className="h-9 w-[300px] flex-row justify-between items-center border py-1 px-3 gap-1 rounded  flex">
       <input
-        placeholder="Search"
+        placeholder="Ingrese busqueda"
         value={searchValue}
         onChange={onSearchValueChange}
         className="text-black font-normal text-sm outline-none w-full h-5 placeholder:text-secondary-200"
       />
-      <SearchIcon />
+      {!visible ? <SearchIcon /> : null}
     </div>
   );
 }
