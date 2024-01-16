bf09d40b7bc3bf7460b52713cd98bb5c93106cfe
Angel Hernandez <64968854+Delta27222@users.noreply.github.com>
2024-01-16 15:09:32 -0800

    Update appointmentConsult.page.jsx

--- src/pages/appointmentConsultPage/appointmentConsult.page.jsx
+++ src/pages/appointmentConsultPage/appointmentConsult.page.jsx
@@ -59,7 +59,7 @@ export const AppointmentConsultPage = () => {
           </div>
         </div>
       </section>
-      <div className='mt-10 w-[70%] mb-20'>
+      <div className='mt-10 w-[70%] mb-28'>
         <TableComponent columns={columnsAppointments1} id_doctor={doctorId} data={appointments} action={'appointments'} path={'../appointmentForm/update'}/>
       </div>
       <FooterComponent />
